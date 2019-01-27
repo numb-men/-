ae7d5d3666d6e382c6e3207c8a0595a4f150c935
numb-men <le@hengyumo.cn>
2019-01-27 20:12:27 +0800

    rewrite private policy page

--- pages/consignee-manage/consignee-manage.js
+++ pages/consignee-manage/consignee-manage.js
@@ -1,9 +1,7 @@
 // pages/consignee-manage/consignee-manage.js
 var app = getApp()
 Page({
-  /**
-   * 页面的初始数据
-   */
+
   data: {
     //判断是否删除（显示对话框）
     isdelete:false,
@@ -100,53 +98,4 @@ Page({
       })
     }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/edit-consignee/edit-consignee.js
+++ pages/edit-consignee/edit-consignee.js
@@ -2,9 +2,6 @@
 var app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     //保存时的参数
     name:"",
@@ -132,9 +129,6 @@ Page({
     console.log(this.data.default_position);
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     var position_list_height = 480* app.globalData.rpx_to_px
     //console.log(order_list_height, app.globalData.screenHeight)
@@ -162,53 +156,4 @@ Page({
       })
     }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
